342165ae59d9d63fb6201e4776cafe7dacaca10c
Jonathan Springer <springermac@gmail.com>
2014-02-22 03:23:00 -0500

    Attempt to fix monitor audio check box not saving state.

--- audio/ui/settingstabform.py
+++ audio/ui/settingstabform.py
@@ -17,11 +17,15 @@ class SettingsTab(QtGui.QWidget, Ui_settingsTab):
     def loadsettings(self):
         print(1)
         settings = QtCore.QSettings()
-        self.monitorAudio.setCheckState(settings.value("MonitorCheckBox").toBool())
         print(settings.value("MonitorCheckBox").toBool())
+        print(self.monitorAudio.isChecked())
+        self.monitorAudio.setChecked(settings.value("MonitorCheckBox").toBool())
+        print(self.monitorAudio.isChecked())
 
     def savesettings(self):
         print(2)
         settings = QtCore.QSettings()
+        print(settings.value("MonitorCheckBox").toBool())
+        print(self.monitorAudio.isChecked())
         settings.setValue("MonitorCheckBox", QtCore.QVariant(self.monitorAudio.isChecked()))
         print(self.monitorAudio.isChecked())
